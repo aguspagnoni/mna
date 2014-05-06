84d12fd57373cbb544ef4ab8bc52d39a04ef4614
Agustin Pagnoni <aguspagnoni@hotmail.com>
2014-05-06 00:40:55 -0300

    brute compressing octave file

---
+++ tpe1/brute.m
@@ -0,0 +1,8 @@
+a = imread('lena512.bmp');
+%imshow(a) % muestro la imagen original
+b = im2col(a,[16,16],'distinct');
+m = mean(double(b));
+m = m.';
+M = repmat(m,1,1024);
+d = col2im(M,[16,16],[512,512],'distinct'); meanimg = uint8(round(d));
+imshow(meanimg) % muestro la imagen ‘‘media’’
