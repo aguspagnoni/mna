289d733446b0a00da62a80d49da828a6ec4aa052
Agustin Pagnoni <aguspagnoni@hotmail.com>
2014-05-06 01:38:18 -0300

    doc

--- tpe1/brute.m
+++ tpe1/brute.m
@@ -3,6 +3,7 @@ a = imread('lena512.bmp');
 b = im2col(a,[16,16],'distinct');
 m = mean(double(b));
 m = m.';
-M = repmat(m,1,1024);
-d = col2im(M,[16,16],[512,512],'distinct'); meanimg = uint8(round(d));
+M = repmat(m,1,256);
+d = col2im(M',[16,16],[512,512],'distinct');
+meanimg = uint8(round(d));
 imshow(meanimg) % muestro la imagen ‘‘media’’
